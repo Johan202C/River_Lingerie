607c0a97e687a271b595e390caa6cef0c5ed7381
Johan202C <49340642+Johan202C@users.noreply.github.com>
2020-03-18 14:22:01 -0500

    chore:Se se actualiza mysql y se suben tablas de river lingerie.

--- Desarrollo/Mysql/river_lingerie.sql
+++ Desarrollo/Mysql/river_lingerie.sql
@@ -34,11 +34,24 @@ nom_tipo_pago varchar (40),
 primary key (id_tipo_pago)
 );
 
-create table referencias (
-id_referencias tinyint (3),
+create table referencia(
+id_referencia tinyint (3),
 nom_referencia varchar (50) not null,
-valor float (10) not null,
-primary key (id_referencias)
+primary key (id_referencia)
+);
+
+create table rol(
+id_rol tinyint (3),
+nombre_rol varchar(30),
+primary key (id_rol)
+);
+
+create table almacen(
+id_almacen tinyint (3),
+nombre_almacen varchar(20),
+direccion_alm varchar(30),
+tipo_alm varchar (20),
+primary key (id_almacen)
 );
 
 
@@ -54,5 +67,52 @@ foto blob,
 correo_electronico varchar (40),
 fk_genero tinyint (3),
 fk_tipo_documento tinyint (3),
-foreign key (fk_genero) references genero (id_genero)
+fk_rol tinyint (3),
+fk_almacen tinyint (3),
+foreign key (fk_genero) references genero (id_genero),
+foreign key (fk_tipo_documento) references tipo_documento(id_tipo_documento),
+foreign key (fk_almacen) references almacen (id_almacen),
+foreign key (fk_rol) references rol (id_rol),
+primary key (id_numero_documento,fk_tipo_documento,fk_rol)
 );
+
+create table pedido (
+id_pedido tinyint (3),
+nombre_pedido varchar (30),
+fk_cliente varchar (25),
+foreign key (fk_cliente) references cliente (id_numero_documento),
+primary key (id_pedido,fk_cliente)
+);
+
+create table producto(
+id_producto tinyint (3),
+valor float(15),
+stock int(5),
+estado boolean,
+fk_referencia tinyint (3),
+fk_talla tinyint (3),
+fk_color tinyint (3),
+foreign key (fk_referencia) references referencia (id_referencia),
+foreign key (fk_talla) references talla (id_talla),
+foreign key (fk_color) references color (id_color),
+primary key (id_producto)
+);
+
+create table factura(
+id_factura tinyint (5),
+fecha date,
+hora_despacho time,
+iva varchar(10),
+valor_subtotal varchar (10),
+valor_total varchar (10),
+fk_pedido tinyint (3),
+fk_producto tinyint (3),
+fk_id_numero_documento varchar (25),
+fk_tipo_pago tinyint (3),
+foreign key (fk_pedido) references pedido (id_pedido),
+foreign key (fk_producto) references producto (id_producto),
+foreign key (fk_id_numero_documento) references cliente (id_numero_documento),
+foreign key (fk_tipo_pago) references tipo_pago (id_tipo_pago),
+primary key (id_factura,fk_tipo_pago)
+);
+
